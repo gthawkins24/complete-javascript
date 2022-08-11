20029fce229b295c39441ff27ef4f1dfa1e054b3
gthawkins24 <gthawkins24@gmail.com>
2022-08-11 17:46:53 -0400

    added functionality for 'again' button

--- script.js
+++ script.js
@@ -9,9 +9,9 @@
 // document.querySelector('.guess').value = 23;
 // console.log(document.querySelector('.guess').value);
 
-const secretNumber = Math.trunc((Math.random()*20)+1);
+let secretNumber = Math.trunc((Math.random()*20)+1);
 let score = 20;
-document.querySelector('.number').textContent = secretNumber;
+
 
 document.querySelector('.check').addEventListener('click', function() {
   const guess = Number(document.querySelector('.guess').value);
@@ -22,6 +22,7 @@ document.querySelector('.check').addEventListener('click', function() {
     // When player wins
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = '🎉 Correct Number!';
+    document.querySelector(".number").textContent = secretNumber;
     document.querySelector("body").style.backgroundColor = "#60b347";
 
     document.querySelector('.number').style.width = '30rem';
@@ -47,4 +48,16 @@ document.querySelector('.check').addEventListener('click', function() {
       document.querySelector(".score").textContent = 0;
     }
   }
+});
+
+document.querySelector('.again').addEventListener('click', function() {
+  score = 20;
+  secretNumber = Math.trunc(Math.random() * 20 + 1);
+
+  document.querySelector(".message").textContent = "Start Guessing...";
+  document.querySelector('.score').textContent = score;
+  document.querySelector(".number").textContent = "?";
+  document.querySelector(".guess").value = " ";
+  document.querySelector("body").style.backgroundColor = "#222";
+  document.querySelector(".number").style.width = "15rem";
 });
